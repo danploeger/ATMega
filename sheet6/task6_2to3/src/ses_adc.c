00ac3fdf81cd0cefccb92bf2a8631d13b262adc7
Daniel Plö <dp.service@posteo.de>
2014-06-03 15:18:03 +0200

    tasks_6_2to3 initial commit

---
+++ sheet6/task6_2to3/src/ses_adc.c
@@ -0,0 +1,28 @@
+/* INCLUDES ******************************************************************/
+#include "ses_adc.h"
+#include "ses_common.h"
+
+/* DEFINES & MACROS **********************************************************/
+
+/* PRIVATE VARIABLES *********************************************************/
+
+/* FUNCTION DEFINITION *******************************************************/
+
+void adc_init(void) {
+
+}
+
+uint16_t adc_read(uint8_t adc_channel) {
+
+}
+
+
+void adc_disable(void) {
+
+}
+
+
+int16_t adc_convertTemp(uint16_t val) {
+
+}
+
