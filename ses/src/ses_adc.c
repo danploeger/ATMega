af679476d6f144dbe0de5216d968343f4d8cd72b
Daniel Plö <dp.service@posteo.de>
2014-06-17 22:53:35 +0200

    ses adc implementation continued (work in progress)

--- ses/src/ses_adc.c
+++ ses/src/ses_adc.c
@@ -2,6 +2,7 @@
 #include "ses_adc.h"
 #include "ses_common.h"
 #include <util/atomic.h>
+#include <avr/sleep.h>
 
 /* DEFINES & MACROS **********************************************************/
 
@@ -35,18 +36,51 @@ void adc_init(void) {
 
 	// Step 5: writing of ADC Control and Status Register A
 	ADCSRA = ((1 << ADEN) | (0 << ADSC) | (0 << ADATE) | (1 << ADIF) | (1 << ADIE) | (ADC_PRESCALE << ADPS0));
-	// TODO: SREG I-bit?
 }
 
 uint16_t adc_read(uint8_t adc_channel) {
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+	set_sleep_mode(SLEEP_MODE_ADC);
 
+	// select adc channel
+	ADCSRB &= ~(1 << MUX5);
+	switch (adc_channel) {
+	case ADC_MIC_NEG_CH:
+		ADMUX &= ~((1 << MUX4) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+		ADMUX |= PIN(1 << ADC_MIC_NEG_PORT); // TODO: ?
+		break;
+	case ADC_MIC_POS_CH:
+		break;
+	case ADC_TEMP_CH:
+		break;
+	case ADC_RESERVED1_CH:
+		break;
+	case ADC_LIGHT_CH:
+		break;
+	case ADC_JOYSTICK_CH:
+		break;
+	case ADC_RESERVED2_CH:
+		break;
+	case ADC_RESERVED3_CH:
+		break;
+	case ADC_NUM:
+		break;
+	default:
+		return ADC_INVALID_CHANNEL;
 	}
+
+	// start conversion
+	sleep_enable();
+	do {
+		sleep_cpu();
+	} while ((ADCSRA & (1 << ADSC)) != 0);
+	sleep_disable();
+
+	return ADC;
 }
 
 
 void adc_disable(void) {
-
+	ADCSRA &= ~(1 << ADEN); // TODO: if-Bedingung (atomic)
 }
 
 
@@ -54,3 +88,7 @@ int16_t adc_convertTemp(uint16_t val) {
 
 }
 
+ISR(ADC_vect)
+{
+
+}
