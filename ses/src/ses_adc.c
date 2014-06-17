ab25e97584dbe912d58007a9038128a2e3de6229
Daniel Plö <dp.service@posteo.de>
2014-06-17 16:40:42 +0200

    ses adc implementation (work in progress)

--- ses/src/ses_adc.c
+++ ses/src/ses_adc.c
@@ -1,6 +1,7 @@
 /* INCLUDES ******************************************************************/
 #include "ses_adc.h"
 #include "ses_common.h"
+#include <util/atomic.h>
 
 /* DEFINES & MACROS **********************************************************/
 
@@ -9,11 +10,38 @@
 /* FUNCTION DEFINITION *******************************************************/
 
 void adc_init(void) {
+	// Step 1: configuration of data direction registers and
+	// deactivation of pull-up resistors
+	DDRF  &=~(1 << ADC_TEMP_PIN);
+	DDRF  &=~(1 << ADC_LIGHT_PIN);
+	DDRF  &=~(1 << ADC_MIC_POS_PIN);
+	DDRF  &=~(1 << ADC_MIC_NEG_PIN);
+	DDRF  &=~(1 << ADC_JOYSTICK_PIN);
 
+	PORTF |= (1 << ADC_TEMP_PIN);
+	PORTF |= (1 << ADC_LIGHT_PIN);
+	PORTF |= (1 << ADC_MIC_POS_PIN);
+	PORTF |= (1 << ADC_MIC_NEG_PIN);
+	PORTF |= (1 << ADC_JOYSTICK_PIN);
+
+	// Step 2: disabling power on ADC
+	PRR0 &=  ~(1 << PRADC);
+
+	// Step 3: reference voltage set
+	ADMUX |= (ADC_VREF_SRC << REFS0);
+
+	// Step 4: A/D conversion right adjust result
+	ADMUX &= ~(1 << ADLAR);
+
+	// Step 5: writing of ADC Control and Status Register A
+	ADCSRA = ((1 << ADEN) | (0 << ADSC) | (0 << ADATE) | (1 << ADIF) | (1 << ADIE) | (ADC_PRESCALE << ADPS0));
+	// TODO: SREG I-bit?
 }
 
 uint16_t adc_read(uint8_t adc_channel) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
 
+	}
 }
 
 
