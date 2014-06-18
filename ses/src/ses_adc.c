3724bdc13ea7d56c0623023067cc1438b30b4009
Daniel Plö <dp.service@posteo.de>
2014-06-18 10:48:41 +0200

    ses adc implementation finished (not tested yet)

--- ses/src/ses_adc.c
+++ ses/src/ses_adc.c
@@ -36,6 +36,9 @@ void adc_init(void) {
 
 	// Step 5: writing of ADC Control and Status Register A
 	ADCSRA = ((1 << ADEN) | (0 << ADSC) | (0 << ADATE) | (1 << ADIF) | (1 << ADIE) | (ADC_PRESCALE << ADPS0));
+
+	// Ensure start-up waiting time of 20 us
+	_delay_us(20);
 }
 
 uint16_t adc_read(uint8_t adc_channel) {
@@ -43,27 +46,34 @@ uint16_t adc_read(uint8_t adc_channel) {
 
 	// select adc channel
 	ADCSRB &= ~(1 << MUX5);
+	ADMUX &= ~((1 << MUX4) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
 	switch (adc_channel) {
 	case ADC_MIC_NEG_CH:
-		ADMUX &= ~((1 << MUX4) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
-		ADMUX |= PIN(1 << ADC_MIC_NEG_PORT); // TODO: ?
+		ADMUX |= (1 << ADC_MIC_NEG_CH);
 		break;
 	case ADC_MIC_POS_CH:
+		ADMUX |= (1 << ADC_MIC_POS_CH);
 		break;
 	case ADC_TEMP_CH:
+		ADMUX |= (1 << ADC_TEMP_CH);
 		break;
 	case ADC_RESERVED1_CH:
+		ADMUX |= (1 << ADC_RESERVED1_CH);
 		break;
 	case ADC_LIGHT_CH:
+		ADMUX |= (1 << ADC_LIGHT_CH);
 		break;
 	case ADC_JOYSTICK_CH:
+		ADMUX |= (1 << ADC_JOYSTICK_CH);
 		break;
 	case ADC_RESERVED2_CH:
+		ADMUX |= (1 << ADC_RESERVED2_CH);
 		break;
 	case ADC_RESERVED3_CH:
+		ADMUX |= (1 << ADC_RESERVED3_CH);
 		break;
 	case ADC_NUM:
-		break;
+		return ADC_NUM;
 	default:
 		return ADC_INVALID_CHANNEL;
 	}
@@ -71,8 +81,8 @@ uint16_t adc_read(uint8_t adc_channel) {
 	// start conversion
 	sleep_enable();
 	do {
-		sleep_cpu();
-	} while ((ADCSRA & (1 << ADSC)) != 0);
+		sleep_cpu(); // go to sleep (and trigger ADC conversion)
+	} while ((ADCSRA & (1 << ADSC)) != 0); // validate that the interrupt is triggered by ADC
 	sleep_disable();
 
 	return ADC;
@@ -80,7 +90,7 @@ uint16_t adc_read(uint8_t adc_channel) {
 
 
 void adc_disable(void) {
-	ADCSRA &= ~(1 << ADEN); // TODO: if-Bedingung (atomic)
+	ADCSRA &= ~(1 << ADEN);
 }
 
 
