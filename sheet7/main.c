609544c715decfd9982cf1a6e97b53634a7c8b43
Daniel Plö <dp.service@posteo.de>
2014-07-04 12:53:14 +0200

    main() and init()

--- sheet7/main.c
+++ sheet7/main.c
@@ -22,38 +22,64 @@ struct Fsm {
 	State state;		// the current state
 	uint8_t alarmSet;	// is alarm enabled?
 	uint32_t timeSet;	// temporary buffer for time
-
 };
-
-
 
 struct Event {
 	uint8_t signal;		// multi purpose signal
 };
 
-
-
-
-
 /* dispatches events to state machine, called in application */
-inline static void fsm_dispatch(Fsm *fsm, const Event* event);
-
-/* sets and calls initial state of state machine */
-inline static void fsm_init(Fsm *fsm, State init);
-
-
-void alarmClock_init(Fsm *me, Event *e) {
-	// initialize hardware
-
-	e->signal = EVENT_NONE;
-	me->signal = (State) alarmClock_YOUR_NEXT_STATE;
+inline static void fsm_dispatch(Fsm *fsm, const Event* event) {
+	fsm->state(fsm, event);
 }
 
+/* sets and calls initial state of state machine */
+inline static void fsm_init(Fsm *fsm, State init) {
+	fsm->state = init;
+	fsm_dispatch(fsm, NULL);
+}
+
+void joystickCallback() {
+	Event e;
+	e->signal = JOYSTICK_SIG;
+	fsm_dispatch((Fsm*) & keyboard, (const Event*) &e);
+}
+
+void rotaryCallback() {
+	Event e;
+	e->signal = ROTARY_SIG;
+	fsm_dispatch((Fsm*) & keyboard, (const Event*) &e);
+}
+
+/* all sub-initializations of hardware and fsm initial state */
+void alarmClock_init(Fsm *me, Event *e) {
+	/** initialize hardware **/
+	/* initialize LEDs */
+	leds_init();
+	leds_off();
+	/* initialize display */
+	lcd_init();
+	stdout = lcdout;
+	/* start scheduler */
+	scheduler_init();
+	/* initialize buttons */
+	button_init();
+	button_setJoystickButtonCallback(joystickCallback);
+	button_setRotaryButtonCallback(rotaryCallback);
+	/* Globally enable Interrupts */
+	sei();
+
+	/* initialize fsm */
+	// TODO set timeStruct
+	e->signal = EVENT_NONE;
+	me->state = (State) alarmClock_setClockHour;
+}
 
 int main(void) {
-
-
-
-
+	Fsm fsm;
+	fsm_init((Fsm*) &fsm, (void*)alarmClock_init);
+	while (1) {
+		scheduler_run();
+	}
 	return 0;
 }
